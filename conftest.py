a43332c76a7b5257c0bf2056afe3ab9392a67534
Ekaterina Karelova <kulibinka1987@gmail.com>
2025-04-09 10:04:48 +0300

    Fix: созданный архив кладем в папку resources

--- conftest.py
+++ conftest.py
@@ -10,10 +10,13 @@ def create_archive():
     file_names = ['Test_CSV.csv',
                   'Test_Excel.xlsx',
                   'Test_Pdf.pdf']
+    dir_path = os.getcwd()
+    file_path = os.path.join(dir_path, 'files')
 
-    file_path = os.path.join(os.getcwd(), 'files')
-
-    with zipfile.ZipFile('archive_files.zip', 'w') as zf:
+    if not os.path.exists(os.path.join(dir_path, 'resources')):
+        os.mkdir(os.path.join(dir_path, 'resources'))
+    with zipfile.ZipFile(os.path.join(dir_path, 'resources', 'archive_files.zip'), 'w') as zf:
         for file in file_names:
             add_file = os.path.join(file_path, file)
             zf.write(add_file, os.path.basename(add_file))
+
